c6af7f9d826889b34807609aed35e5476f3d4479
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2023-02-09 15:45:13 +0100

    Udated SQL queries

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- Create users table it no exists
-CREATE table IF NOT EXISTS user(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255)
-);
+-- script that creates a table users with follwing fields
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
